2e5362cd6ae90d0ec26e863d31f8a2eb821a5ce6
HomePC <grawires@gmail.com>
2023-01-06 16:25:39 +0100

    docker-compose for all project

--- 1DCutter-Front/karma.conf.js
+++ 1DCutter-Front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/OneDCutter-A'),
+      dir: require('path').join(__dirname, './coverage/onedcutter-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- 1DCutter-Front/server.js
+++ 1DCutter-Front/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/OneDCutter-A'));
+app.use(express.static('./dist/onedcutter-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/OneDCutter-A/'}),
+    res.sendFile('index.html', {root: 'dist/onedcutter-front/'}),
 );
 
 // Start the app by listening on the default Heroku port
