0139b667b274714d02bfd54c8cda16c78ffbe3ba
HomePC <grawires@gmail.com>
2023-01-11 22:44:14 +0100

    node build time reduced from 180 to 20s

--- 1DCutter-Front/server.js
+++ 1DCutter-Front/server.js
@@ -1,15 +1,18 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// NEEDED FOR HEROKU DEPLOYMENT
 
-const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/onedcutter-front'));
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/onedcutter-front/'}),
-);
+// const app = express();
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// // Serve only the static files form the dist directory
+// app.use(express.static('./dist/onedcutter-front'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/onedcutter-front/'}),
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
