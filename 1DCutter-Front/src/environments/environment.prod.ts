931d70711929c73d3142406feccd60570c3c8505
JobLaptok <grawires@gmail.com>
2023-01-13 18:06:17 +0100

    trying to get ssl work with api

--- 1DCutter-Front/src/environments/environment.prod.ts
+++ 1DCutter-Front/src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   // API_URL: 'https://onedcutter.herokuapp.com'
   // API_URL: 'http://vps-b5ffb21c.vps.ovh.net:8080'
-  API_URL: 'http://cutter.grawires.pl:8080'
+  // API_URL: 'https://cutter.grawires.pl:8443'
+  API_URL: 'https://api.cutter.grawires.pl:8443'
+  // API_URL: 'http://cutter.grawires.pl:8080'
   // API_URL: 'http://localhost:8080'
 };
